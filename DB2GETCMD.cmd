a0dd00d10661be19d5d331200ef136fafb81fba4
Liam <WorksOfBarry@users.noreply.github.com>
2016-05-24 22:39:14 +0100

    Create DB2GETCMD.cmd

---
+++ DB2GETCMD.cmd
@@ -0,0 +1,7 @@
+             /* Build this over DB2GET *PGM */
+
+             CMD        Prompt('DB2 Download')
+             PARM       KWD(PLOC)  TYPE(*CHAR) LEN(128) PROMPT('Direct URL')
+             PARM       KWD(PPTH)  TYPE(*CHAR) LEN(128) PROMPT('IFS / Filename.ext')
+
+ 
